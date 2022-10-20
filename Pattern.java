cf791c8ca4277db9251b4aa1314c85ecb4349157
anuLoveCoading <anusrivas07@gmail.com>
2022-10-20 19:35:54 +0530

    starPattern

--- Pattern.java
+++ Pattern.java
@@ -7,17 +7,32 @@ public class Pattern {
 
         System.out.println("Enter Your Number :");
         int n = scn.nextInt();
-        int space = 0 ,star = n;
-        for(int i=1;i<=n;i++){
-           for(int j=1;j<=space;j++){
-            System.out.print("\t");
-           }
-           for(int j=1;j<=star;j++){
-            System.out.print("*\t");
-           }
+        // int space = 0 ,star = n;
+        // for(int i=1;i<=n;i++){
+        //    for(int j=1;j<=space;j++){
+        //     System.out.print("\t");
+        //    }
+        //    for(int j=1;j<=star;j++){
+        //     System.out.print("*\t");
+        //    }
 
-            space++;
-            star--;
+        //     space++;
+        //     star--;
+        //     System.out.println();
+        // }
+
+        int space = n-1 , star = 0;
+
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print("\t");
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print("*\t");
+            }
+            space--;
+            star++;
             System.out.println();
         }
 

0d25a2587aa9b96e9b92e428be932e270c359499
anuLoveCoading <anusrivas07@gmail.com>
2022-10-20 12:07:29 +0530

    pallindrome

---
+++ Pallindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pallindrome {
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+
+        System.out.println("Enter Your Input:");
+        int N = scn.nextInt();
+        for(int i=1;i<=N;i++){
+            int sum =0 ;
+            int n = i;
+             for(; n>=1;n/=10){
+                int rem = n%10;
+                sum =(sum*10)+rem;
+    
+             }
+             if(i==sum){
+                System.out.println("Yes it is :");
+                System.out.println(i+ " Pallindorme");
+             }
+        }
+       
+    }
+}

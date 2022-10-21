88a32551b53afdb8c4156b8cd580f937f56bc7a2
anuLoveCoading <anusrivas07@gmail.com>
2022-10-21 18:08:23 +0530

    Pattern

--- Pattern.java
+++ Pattern.java
@@ -5,36 +5,56 @@ public class Pattern {
         Scanner scn = new Scanner(System.in);
 
 
-        System.out.println("Enter Your Number :");
-        int n = scn.nextInt();
+        // System.out.println("Enter Your Number :");
+        // int n = scn.nextInt();
         // int space = 0 ,star = n;
         // for(int i=1;i<=n;i++){
         //    for(int j=1;j<=space;j++){
-        //     System.out.print("\t");
+        //       System.out.print("\t");
         //    }
-        //    for(int j=1;j<=star;j++){
-        //     System.out.print("*\t");
-        //    }
+        //         for(int j=1;j<=star;j++){
+        //             System.out.print("*\t");
+        //         }
 
         //     space++;
         //     star--;
         //     System.out.println();
         // }
 
-        int space = n-1 , star = 0;
 
+        // print the stair;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=space;j++){
-                System.out.print("\t");
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*\t");
+        //     }
+        //     System.out.println();
+        // }
+
+        // print the reverse stair;
+
+        // for(int i = 5;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*\t");
+
+        //     }
+        //     System.out.println();
+        // }
+
+        // print the stair of  sum digit of star;
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                if(j%2==0){
+                    System.out.print(j+"\t");
+                }else{
+                    System.out.print("*\t");
+                }
+                
             }
-            for(int j=1;j<=star;j++){
-                System.out.print("*\t");
-            }
-            space--;
-            star++;
             System.out.println();
         }
 
+       
     }
 }

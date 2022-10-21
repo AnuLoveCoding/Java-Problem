d8a64261397df0c34bec8337af4aeb45f098268e
anuLoveCoading <anusrivas07@gmail.com>
2022-10-21 18:42:09 +0530

    Pattern

--- Pattern.java
+++ Pattern.java
@@ -5,9 +5,26 @@ public class Pattern {
         Scanner scn = new Scanner(System.in);
 
 
-        // System.out.println("Enter Your Number :");
-        // int n = scn.nextInt();
-        // int space = 0 ,star = n;
+        System.out.println("Enter Your Number :");
+        int n = scn.nextInt();
+
+        // int space = n-1;
+        // int star = 1;
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=space;j++){
+        //         System.out.print("\t");
+        //     }
+        //     for(int j=1;j<=star;j++){
+        //         System.out.print("*\t");
+        //     }
+        //     space--;
+        //     star++;
+        //     System.out.println();
+        // }
+
+        // int space = 0 ;
+        // int star = n;
         // for(int i=1;i<=n;i++){
         //    for(int j=1;j<=space;j++){
         //       System.out.print("\t");
@@ -69,7 +86,8 @@ public class Pattern {
         //     System.out.println();
         // }
        
-        
+        // int n =5;
+        // int space
     
 
 

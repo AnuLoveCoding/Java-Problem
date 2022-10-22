eeedc9bdc1d89d47d8376815249033aef1713927
anuLoveCoading <anusrivas07@gmail.com>
2022-10-23 00:02:24 +0530

    swtich-Case

--- switch_case.java
+++ switch_case.java
@@ -5,6 +5,28 @@ public class switch_case {
 
         Scanner scn = new Scanner(System.in);
         
+        System.out.println("Enter Your Fruit Name : ");
+        String fruit = scn.nextLine();
+
+        switch (fruit) {
+            case "mango": System.out.println("king of fruit");
+                
+                break;
+            case "gauava" : System.out.println("Very Tasty");
+                break;
+            case "grapes" : System.out.println("very tasty fruit");
+                 break;
+            case "pine-apple" : System.out.println("pine-apple");
+                break;
+            case "apple" : System.out.println("Apple");
+                break;
+
+                
         
+
+        
+            default: System.out.println("Wrong Input");
+                break;
+        }
     }
 }

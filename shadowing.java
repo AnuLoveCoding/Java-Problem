0c896f83f26bd17be3c7c0175544bd5716be9ed5
anuLoveCoading <anusrivas07@gmail.com>
2022-10-23 16:36:02 +0530

    shadowing

--- shadowing.java
+++ shadowing.java
@@ -1,14 +1,16 @@
+import java.util.Arrays;
+
 public class shadowing {
-   static  int x = 90;
+//    static  int x = 90;
     public static void main(String[] args) {
         // System.out.println(x);
-        int x = 40;
-        System.out.println(x);
-        
-        num();
+        // int x = 40;
+        // System.out.println(x);
+
+        num(2,3,4,5,6,7,8,9,5,4,5,4);
     }
 
-    static void num(){
-        System.out.println(x);
+    static void num(int...v){
+        System.out.println(Arrays.toString(v));
     }
 }

184cd89ed431754dc76efe7dec5f7d5eef27f0ee
anuLoveCoading <anusrivas07@gmail.com>
2022-10-23 14:42:19 +0530

    function

--- function.java
+++ function.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 
 public class function {
     public static void main(String[] args) {
-        sum();
+        // sum();
+        String name = "Anurag";
+        greet(name);
+
 
     }
-    //  Q. sum the two numbers;
-    static void sum(){
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Number 1 ");
-        int num1 = scn.nextInt();
-        System.out.println("Enetr  Number 2 ");
-        int num2 = scn.nextInt();
-        System.out.println("Sum of two number ");
-        int sum = num1 + num2 ;
-        System.out.println(" Sum is "+sum);
+
+    static void greet(String naam){
+        System.out.println(naam);
     }
+
+    //  Q. sum the two numbers;
+    // static void sum(){
+    //     Scanner scn = new Scanner(System.in);
+    //     System.out.println("Enter Number 1 ");
+    //     int num1 = scn.nextInt();
+    //     System.out.println("Enetr  Number 2 ");
+    //     int num2 = scn.nextInt();
+    //     System.out.println("Sum of two number ");
+    //     int sum = num1 + num2 ;
+    //     System.out.println(" Sum is "+sum);
+    // }
     
 }

6f8557e2216741ff4fbb4adf1f1e7152d29fe86c
anuLoveCoading <anusrivas07@gmail.com>
2022-10-24 12:09:43 +0530

    Array-class

--- Array.java
+++ Array.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+
+        // int[] arr =  new int[5];
+        // // int[] arr = 
+        // System.out.print(arr[0]+" ");
+        // System.out.print(arr[1]+" ");
+        // System.out.print(arr[2]+" ");
+        // System.out.print(arr[3]+" ");
+        // System.out.print(arr[4]+" ");
+        // System.out.print(arr[5]+" ");
+
+
+        float [] arr = {5,4,3,2,1,5,6.5f};
+
+        System.out.println(arr[6]);
     }
 }

65c1ae6cf5e758becdfd3397ebb7d11e344fbb15
anuLoveCoading <anusrivas07@gmail.com>
2022-10-24 13:17:52 +0530

    2DArray

--- IIArray.java
+++ IIArray.java
@@ -1,14 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class IIArray {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
+        int [] num = {4,5,5,6,54,9,4};
+
+        System.out.println(Arrays.toString(num));
+
+
+        raj(num);
+        System.out.println(Arrays.toString(num));
 
 
 
 
 
-        
+    }
+
+    static void raj(int[] num){
+         num[3] = 99;
     }
 }
--- tempCodeRunnerFile.java
+++
@@ -1,3 +0,0 @@
-for (int i = 0; i < arr.length; i++) {
-    //      System.out.print(arr[i]+"-"); // this statement for print the value arr[i];
-    //   }

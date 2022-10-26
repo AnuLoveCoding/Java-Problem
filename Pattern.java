510b9e2eec4fa0feb3bcda716b986edef8b22d40
anuLoveCoading <anusrivas07@gmail.com>
2022-10-26 20:54:39 +0530

    Pattern-print

--- Pattern.java
+++ Pattern.java
@@ -135,21 +135,46 @@ public class Pattern {
         //     System.out.println();
         // }
 
+        // int n = 5;
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(i==j){
+
+        //             System.out.print("*\t");
+        //         }else{
+        //             System.out.print("\t");
+        //         }
+               
+        //     }
+        //     System.out.println();
+        // }
+
+
+
         int n = 5;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==j){
-
+        for (int i = n; i>=1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if( i==j){
                     System.out.print("*\t");
                 }else{
                     System.out.print("\t");
                 }
-               
             }
             System.out.println();
         }
 
+        // for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if( i== j || i+j == n+1){
+        //             System.out.print("*\t");
+        //         }else{
+        //             System.out.print("\t");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
 
     }
 }

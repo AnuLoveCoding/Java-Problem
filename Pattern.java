4e927dd56d566160589b50205af907344a0bb5cb
anuLoveCoading <anusrivas07@gmail.com>
2022-10-27 20:57:07 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -113,27 +113,27 @@ public class Pattern {
 
 
         // int n = 3 ;
-        int star = n/2+1 ,space = 1;
+        // int star = n/2+1 ,space = 1;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=star;j++){
-                System.out.print("*\t");
-            }
-            for(int j=1;j<=space;j++){
-                System.out.print("\t");
-            }
-            for(int j=1;j<=star;j++){
-                System.out.print("*\t");
-            }
-            if(i<=n/2){
-                star--;
-                space+=2;
-            }else{
-                star++;
-                space-=2;
-            }
-            System.out.println();
-        }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=star;j++){
+        //         System.out.print("*\t");
+        //     }
+        //     for(int j=1;j<=space;j++){
+        //         System.out.print("\t");
+        //     }
+        //     for(int j=1;j<=star;j++){
+        //         System.out.print("*\t");
+        //     }
+        //     if(i<=n/2){
+        //         star--;
+        //         space+=2;
+        //     }else{
+        //         star++;
+        //         space-=2;
+        //     }
+        //     System.out.println();
+        // }
 
         int n = 5;
 

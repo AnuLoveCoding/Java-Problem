84ddf33d9e30f46a7d69b770f583244d77bd3697
anuLoveCoading <anusrivas07@gmail.com>
2022-10-29 20:31:23 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
 
 public class Pattern {
     public static void main(String[] args) {
@@ -188,28 +189,42 @@ public class Pattern {
         //     System.out.println();
         // }
 
+        // int n = 5;
+        // for (int i = n; i>=1; i--) {
+        //     for (int j = 0; j <=i; j++) {
+        //         System.out.print("");
+        //     }
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for (int i = n; i>=1; i--) {
+        //     for (int j = 1; j <= n-i; j++) {
+        //         System.out.print(" ");
+                
+        //     }for (int j = 1; j<= i; j++) {
+        //         System.out.print("*");
+                
+        //     }
+        //     System.out.println();
+        // }
+
         int n = 5;
-        for (int i = n; i>=1; i--) {
-            for (int j = 0; j <=i; j++) {
-                System.out.print("");
-            }
-            for (int j = 1; j <=i; j++) {
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
 
-        for (int i = n; i>=1; i--) {
-            for (int j = 1; j <= n-i; j++) {
-                System.out.print(" ");
-                
-            }for (int j = 1; j<= i; j++) {
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
-                
+            }
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-
 
 
 

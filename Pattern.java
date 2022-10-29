dd6fdb1cd8806c723a60fcca1589c41f66438350
anuLoveCoading <anusrivas07@gmail.com>
2022-10-29 20:37:59 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -217,7 +217,7 @@ public class Pattern {
             for (int j = 1; j <=n-i+1; j++) {
                 System.out.print("*");
             }
-            for (int j = 1; j <= 2*(n-i); j++) {
+            for (int j = 1; j <= 2*(i-1); j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= n-i+1; j++) {

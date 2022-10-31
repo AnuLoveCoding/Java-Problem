810f290dad992db5f0b507063bf1f063e5c2f386
anuLoveCoading <anusrivas07@gmail.com>
2022-10-31 21:48:52 +0530

    Pattern

--- Pattern.java
+++ Pattern.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 
+import javax.sound.midi.Soundbank;
+
 public class Pattern {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -211,38 +213,54 @@ public class Pattern {
         //     System.out.println();
         // }
 
+        // int n = 5;
+
+        // for (int i = 1; i <=n; i++) {
+        //     for (int j = 1; j <=n-i+1; j++) {
+        //         System.out.print("*");
+        //     }
+        //     for (int j = 1; j <= 2*(i-1); j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= n-i+1; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+        // }
+
+        // for (int i = 1; i <=n ; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     for (int j = 1; j <= 2*(n-i); j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
         int n = 5;
 
-        for (int i = 1; i <=n; i++) {
-            for (int j = 1; j <=n-i+1; j++) {
-                System.out.print("*");
-            }
-            for (int j = 1; j <= 2*(i-1); j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= n-i+1; j++) {
-                System.out.print("*");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
-
+            
+            
         }
-
-        for (int i = 1; i <=n ; i++) {
+        for (int i = n; i>=1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            for (int j = 1; j <= 2*(n-i); j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
-
-
-
-
 
 
 

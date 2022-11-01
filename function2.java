d944d86e6c856210e1d35d26ccb5b47b0fdc058a
anuLoveCoading <anusrivas07@gmail.com>
2022-11-01 20:00:07 +0530

    function;

--- function2.java
+++ function2.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
 public class function2 {
-    // write a function to add two numbers;
+    
+    // write a function to multiply three numbers;
 
-    static void sub(int x,int y){
-         int minus =  x-y;
-
-         System.out.println(minus);
+    static int multiply(int a, int b,int c){
+        return a*b*c;
     }
 
 
+
+    // write a function to subtract two numbers;
+    // static void sub(int x,int y){
+    //      int minus =  x-y;
+            // to use void no need to return and print in function and call main function;
+    //      System.out.println(minus);
+    // }
+
+
+    // write a function to add two numbers;
     // static int sum(int a , int b){
     //     return a+b ;
         
@@ -20,7 +29,9 @@ public class function2 {
 
     //   System.out.println(sum(4,5));  
 
-            sub(5,2);
+            // sub(5,2);  to data type always only call function.
+
+            System.out.println(multiply(5, 7, 2));
 
 
 

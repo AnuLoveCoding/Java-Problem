a08678af0e3dd9cdfdc4f133b14b1c4f7c75eb50
anuLoveCoading <anusrivas07@gmail.com>
2022-11-01 19:50:50 +0530

    function

--- function2.java
+++ function2.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
 public class function2 {
+    // write a function to add two numbers;
 
-    
+    static int sum(int a , int b){
+        return a+b ;
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+
+      System.out.println(sum(4,5));  
     }
 }

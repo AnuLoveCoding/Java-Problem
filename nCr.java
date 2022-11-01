383815f5a67359d033dbedeb3b1aaf05f8f50e42
anuLoveCoading <anusrivas07@gmail.com>
2022-11-01 21:41:42 +0530

    nCr

---
+++ nCr.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class nCr {
+    // write a function check_prime or not;
+    public static boolean check_prime(n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0){
+                count++ ;
+            }
+        }
+        System.out.println(count);
+
+        if(count==2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // write a function for nCr factorial;
+    public static int factorial( int x) {
+        int fact =1;
+        for (int i = 1; i <= x; i++) {
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+        System.out.println("Enter Your Number 1");
+        int n = scn.nextInt();
+        System.out.println(check_prime(n));
+        // System.out.println("Enter Your Number 2 ");
+        // int r = scn.nextInt();
+
+        // int ans = factorial(n) / (factorial(n-r) *factorial(r));
+        //  System.out.println(ans);
+
+
+
+
+
+    }
+}

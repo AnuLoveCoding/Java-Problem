82f442540f3b060dc23d486176cf2fdd59e61260
anuLoveCoading <anusrivas07@gmail.com>
2022-11-02 21:03:06 +0530

    prime

---
+++ prime.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * prime
+ */
+public class prime {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+        System.out.println("Enter Your Number");
+        int n = scn.nextInt();
+
+        int ans =0;
+
+        for (int i = 1; i <=n; i++) {
+            int count = 0;
+            for (int j = 1; j <=i; j++) {
+                if(i%j==0){
+                    count++;
+                }
+                if(count>2){
+                    break;
+                }
+            }
+            if(count==2){
+                ans++;
+                System.out.print(ans);
+                
+            }
+        } 
+        System.out.println();
+           System.out.println(ans);
+       
+    }
+}
